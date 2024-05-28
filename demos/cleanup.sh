be0af3959bdbadb616332d0ec1a98db071ae70d2
Simon Krenger <skrenger@redhat.com>
2024-05-28 19:17:48 +0200

    Add kustomization example

--- demos/cleanup.sh
+++ demos/cleanup.sh
@@ -6,6 +6,9 @@ oc delete argocd argocd -n development-argocd
 oc delete namespace development-argocd
 
 # Demo 2
+oc delete application app-of-apps -n openshift-gitops
+oc delete application --all -n openshift-gitops
+oc delete applicationset appset-example -n openshift-gitops 
 
 oc create namespace application-guestbook
 oc label namespace application-guestbook argocd.argoproj.io/managed-by=openshift-gitops
--- demos/demo-2.sh
+++ demos/demo-2.sh
@@ -3,16 +3,17 @@
 #doitlive prompt: [{user}@demo-2 {dir}]$
 #doitlive commentecho: true
 
-# Lets see what we have
-tree ..
+# App-of-Apps, ApplicationSets and Kustomize
+
+tree -I demos/ ..
+google-chrome https://openshift-gitops-server-openshift-gitops.apps.skrenger.sandbox2566.opentlc.com
 
 # Use the Cluster GitOps instance
 oc project openshift-gitops
 oc apply -f ../app-of-apps/app-of-apps.yaml
 
-# Check the Argo CD console
-google-chrome https://openshift-gitops-server-openshift-gitops.apps.skrenger.sandbox2566.opentlc.com
-
 # ApplicationSet example
 cat demo-2/appset.yaml
 oc apply -f demo-2/appset.yaml
+
+# Kustomize
